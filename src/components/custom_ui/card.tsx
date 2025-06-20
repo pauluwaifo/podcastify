8250029c9fc75b139f7b75749862aee8bb9d8762
paul uwaifo <uwaifopaul1@gmail.com>
2025-06-20 14:11:09 +0100

    fix: made card more ui friendly

--- src/components/custom_ui/card.tsx
+++ src/components/custom_ui/card.tsx
@@ -9,7 +9,7 @@ export default function Card({ content, icon: Icon }: CardProps) {
   return (
     <div>
       <div className="bg-[#232323] py-2 px-4 rounded-lg border border-[#2C2C2C] hover:scale-102 transition-scale duration-100 delay-75 ease-in-out shadow-md flex flex-row md:flex-col cursor-pointer items-center md:items-start gap-4 md:gap-0 justify-start md:justify-start">
-        <h2 className="text-white text-xl font-bold mb-2">
+        <h2 className="text-[#999] text-xl font-bold mb-2">
           <Icon />
         </h2>
         <p className="text-[#999999] mb-2">{content}</p>
